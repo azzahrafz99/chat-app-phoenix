174f332cb3df28767bad3761ea88113288dde9b8
Fatimah Az-Zahra <azzahrafz@gmail.com>
2020-08-26 10:31:26 +0700

    Setup deploy

--- config/prod.exs
+++ config/prod.exs
@@ -10,8 +10,24 @@ use Mix.Config
 # which you should run after static files are built and
 # before starting your production server.
 config :chatter, ChatterWeb.Endpoint,
-  url: [host: "example.com", port: 80],
-  cache_static_manifest: "priv/static/cache_manifest.json"
+  load_from_system_env: true,
+  url: [scheme: "https", host: "chatapp-phoenix.herokuapp.com", port: 443],
+  force_ssl: [rewrite_on: [:x_forwarded_proto]],
+  cache_static_manifest: "priv/static/cache_manifest.json",
+  secret_key_base: Map.fetch!(System.get_env(), "SECRET_KEY_BASE")
+
+database_url =
+  System.get_env("DATABASE_URL") ||
+    raise """
+    environment variable DATABASE_URL is missing.
+    For example: ecto://USER:PASS@HOST/DATABASE
+    """
+
+config :chatter, Chatter.Repo,
+  adapter: Ecto.Adapters.Postgres,
+  ssl: true,
+  url: database_url,
+  pool_size: String.to_integer(System.get_env("POOL_SIZE") || "10")
 
 # Do not print debug messages in production
 config :logger, level: :info
